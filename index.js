4089b7dabcebac1b462b84f6be01ab41854db158
ismailtalha <ismailtalha789@gmail.com>
2022-04-06 01:52:39 +0500

    1 file modified

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const once = (fn) => {
-  debugger;
-
-  var instance = function () {
-    return fn();
+  var invoked = false;
+  return function () {
+    if (!invoked) {
+      invoked = true;
+      return fn();
+    }
   };
-  if (!instance) {
-    return instance;
-  }
 };
 
 const task = () => {
-  console.log('invoked');
+  console.log('invoked1');
 };
 
 const taskOnce = once(task);
 taskOnce(); // expect to see `invoked` in the console
+taskOnce();
+taskOnce();
+taskOnce();
+taskOnce();
+taskOnce();
+
+taskOnce();
+taskOnce();
